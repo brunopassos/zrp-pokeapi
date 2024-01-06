48d78a008480e56a74d224bc29ac395ab994d2e6
Bruno Passos <brunopassos_bp@hotmail.com>
2024-01-06 16:16:20 -0300

    feat: implementa dockerização do backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,14 @@
 import { app } from "./app"
 
 const PORT: number = 3333
+const HOST = '0.0.0.0'
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.get("/", (req, res) => {
+    res.send("Server running on Docker")
 })
+
+app.listen(PORT, HOST)
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
